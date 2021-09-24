7d4cc83b2ddbbd50d6380c9830397033261147e1
gaowanlu <2209120827@qq.com>
2021-09-24 12:18:23 +0800

    初始化cmake项目结构

---
+++ src/FileControll/kk.cpp
@@ -0,0 +1,4 @@
+#include"FileControll/kk.h"
+void kk(){
+    std::cout<<"HELLO WORLD\n";
+}
---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+#include"FileControll/kk.h"
+using namespace std;
+int main(int argc,char**argv){
+    cout<<"HELLO WORLD\n";
+    return 0;
+}
